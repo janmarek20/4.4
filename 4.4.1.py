d5445d7b57e124e8363c01050db8e24e39444f11
Jan Marek <janekmvi@gmail.com>
2024-04-17 19:02:29 +0200

    Define functions separately and general code changes

--- 4.4.1.py
+++ 4.4.1.py
@@ -2,22 +2,34 @@ import logging
 
 logging.basicConfig(level=logging.INFO)
 
-def mini_calculator():
+def choose_operation():
     operation = int(input('Podaj działanie, posługując się odpowiednią liczbą: 1 Dodawanie, 2 Odejmowanie, 3 Mnożenie, 4 Dzielenie:'))
 
     num1 = float(input('Podaj składnik 1.'))
     num2 = float(input('Podaj składnik 2.'))
-    if operation == 1:
-        logging.info(f'Dodaję {num1} i {num2}')
-        return num1 + num2
-    elif operation == 2:
-        logging.info(f'Odejmuję {num1} i {num2}')
-        return num1 - num2
-    elif operation == 3:
-        logging.info(f'Mnożę {num1} i {num2}')
-        return num1 * num2
-    elif operation == 4:
-        logging.info(f'Dzielę {num1} i {num2}')
-        return num1 / num2
-    
-print(mini_calculator())
+    return operation, num1, num2
+
+
+def mini_calc(x, n1, n2):
+    if x == 1:
+        logging.info(f'Dodaję {n1} i {n2}')
+        return n1 + n2
+    elif x == 2:
+        logging.info(f'Odejmuję {n1} i {n2}')
+        return n1 - n2
+    elif x == 3:
+        logging.info(f'Mnożę {n1} i {n2}')
+        return n1 * n2
+    elif x == 4:
+        logging.info(f'Dzielę {n1} i {n2}')
+        if n2 != 0:
+            return n1 / n2
+        else:
+            raise ZeroDivisionError("Nie można dzielić przez zero!")
+
+
+# main do flow które się uruchamia
+
+if __name__ == '__main__':
+    opr, num1, num2  = choose_operation()
+    print(mini_calc(opr, num1, num2))
